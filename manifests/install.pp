cc6c2e024a9da5b5c3d544825468d7262fb41c2f
Levent YALCIN <leventyalcin@gmail.com>
2015-11-20 15:07:54 +0000

    pip failure

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,6 +4,18 @@ class s3cmd::install (
     $package_provider
 ){
 
+  if ( ! defined( Package['python-pip'] ) ) {
+    package { 'python-pip':
+      ensure => 'installed',
+    }
+  }
+
+  file { '/usr/bin/pip-python':
+    ensure  => 'link',
+    target  => '/usr/bin/pip',
+    require => Package['python-pip'],
+  }
+
   package { 's3cmd':
       ensure   => 'installed',
       provider => $package_provider,
